b091e9040484328dd980b57622717626004e8aa7
Rauana Carvalho <136634083+rauana-carvalho@users.noreply.github.com>
2025-07-30 01:08:22 -0300

    improve: code structure and readability

--- lista/lista-encadeada/src/main.cpp
+++ lista/lista-encadeada/src/main.cpp
@@ -2,19 +2,20 @@
 #include <iostream>
 
 int main() {
-    std::cout << "-- Início dos Testes da Lista Encadeada --" << std::endl;
+    std::cout << "-- Inicio dos Testes da Lista Encadeada --" << std::endl;
     
-    // criação da lista vazia
+    // criacao da lista vazia
     LinkedList lista;
-    std::cout << "Lista criada." << std::endl;
+    std::cout << "Lista criada\n" << std::endl;
 
-    // verificar se a lista está vazia
-    std::cout << "A lista está vazia? " << (lista.estaVazia() ? "Sim" : "Não") << std::endl;
+    // verificar se a lista esta vazia
+    std::cout << "A lista esta vazia? \n" << (lista.estaVazia() ? "Sim." : "Nao.") << std::endl;
+    std::setlocale(LC_ALL, "en_US.utf8");
 
     // imprimir lista vazia
     lista.imprimir();
 
-    // inserir um elemento em uma determinada posição
+    // inserir um elemento em uma determinada posicao
     std::cout << "\n-- Inserindo elementos --" << std::endl;
     lista.inserir(1, 10);
     lista.imprimir();
@@ -26,45 +27,45 @@ int main() {
     lista.imprimir();
     
     // obter o tamanho da lista
-    std::cout << "Tamanho atual da lista: " << lista.obterTamanho() << std::endl;
-    std::cout << "A lista está vazia? " << (lista.estaVazia() ? "Sim" : "Não") << std::endl;
+    std::cout << "\nTamanho atual da lista " << lista.obterTamanho() << std::endl;
+    std::cout << "A lista esta vazia? " << (lista.estaVazia() ? "Sim." : "Nao.") << std::endl;
 
-    // Obter/modificar o valor do elemento de uma determinada posição na lista
+    // Obter/modificar o valor do elemento de uma determinada posicao na lista
     std::cout << "\n-- Obtendo e modificando elementos --" << std::endl;
-    std::cout << "Elemento na posicao 3: " << lista.obterElemento(3) << std::endl;
-    std::cout << "Modificando elemento na posicao 3 para 99." << std::endl;
+    std::cout << "Elemento na posicao 3 " << lista.obterElemento(3) << std::endl;
+    std::cout << "Modificando elemento na posicao 3 para 99" << std::endl;
     lista.modificarElemento(3, 99);
-    std::cout << "Novo elemento na posicao 3: " << lista.obterElemento(3) << std::endl;
+    std::cout << "Novo elemento na posicao 3 " << lista.obterElemento(3) << std::endl;
     lista.imprimir();
 
-    // retirar um elemento de uma determinada posição.
+    // retirar um elemento de uma determinada posicao
     std::cout << "\n-- Retirando elementos --" << std::endl;
-    std::cout << "Retirando elemento da posicao 1." << std::endl;
+    std::cout << "Retirando elemento da posicao 1" << std::endl;
     lista.retirar(1);
     lista.imprimir();
-    std::cout << "Tamanho atual: " << lista.obterTamanho() << std::endl;
+    std::cout << "Tamanho atual " << lista.obterTamanho() << std::endl;
     
-    std::cout << "Retirando elemento da ultima posicao (3)." << std::endl;
+    std::cout << "Retirando elemento da ultima posicao 3" << std::endl;
     lista.retirar(3);
     lista.imprimir();
-    std::cout << "Tamanho atual: " << lista.obterTamanho() << std::endl;
+    std::cout << "Tamanho atual " << lista.obterTamanho() << std::endl;
 
-    std::cout << "Retirando elemento da posicao 1." << std::endl;
+    std::cout << "Retirando elemento da posicao 1" << std::endl;
     lista.retirar(1);
     lista.imprimir();
 
-    std::cout << "Retirando ultimo elemento." << std::endl;
+    std::cout << "Retirando ultimo elemento" << std::endl;
     lista.retirar(1);
     lista.imprimir();
-    std::cout << "Tamanho final: " << lista.obterTamanho() << std::endl;
-    std::cout << "A lista está vazia? " << (lista.estaVazia() ? "Sim" : "Não") << std::endl;
-    
-    // teste de excesso de posição
+    std::cout << "Tamanho final " << lista.obterTamanho() << std::endl;
+    std::cout << "A lista esta vazia? " << (lista.estaVazia() ? "Sim." : "Nao.") << std::endl;
+
+    // teste de excesso de posicao
     try {
-        std::cout << "\nTentando retirar de uma lista vazia..." << std::endl;
+        std::cout << "\nTentando retirar de uma lista vazia" << std::endl;
         lista.retirar(1);
     } catch (const std::out_of_range& e) {
-        std::cerr << "Capturada excecao esperada: " << e.what() << std::endl;
+        std::cerr << "Capturada excecao esperada " << e.what() << std::endl;
     }
 
     std::cout << "\n-- Fim dos Testes --" << std::endl;
